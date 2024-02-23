569a4969242a8b1ff1fb9190fe5b00add5659903
Emil <kadilli.emil@gmail.com>
2024-02-23 16:54:07 +0100

    create welcome component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup>
-import Navigation from './components/Navigation.vue';
-</script>
-
 <template>
   <Navigation />
+  <Welcome />
 </template>
+
+<script setup>
+import Navigation from './components/Navigation.vue';
+import Welcome from './components/Welcome.vue';
+</script>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="bg-white">
+        <div class="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20">
+            <div class="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-40">
+            <div class="px-6 lg:px-0">
+                <div class="mx-auto max-w-2xl">
+                <div class="max-w-lg">
+                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Welcome to my digital space! </h1>
+                    <p class="mt-6 text-lg leading-7 text-gray-600"> Here, I invite you to delve into the world of code and creativity, where lines of logic and pixels come together in a dance of innovation. Join me on this journey as I share my projects, insights, and reflections on the exciting realms of software and game development.</p>
+                    <div class="mt-10 flex items-center gap-x-6">
+                    <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">About Me</a>
+                    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Follow me on GitHub <span aria-hidden="true">→</span></a>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="mt-20 sm:mt-24 md:mx-auto md:max-w-2xl lg:mx-0 lg:mt-0 lg:w-screen">
+                <div class="absolute inset-y-0 right-1/2 -z-10 -mr-10 w-[200%] skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 md:-mr-20 lg:-mr-36" aria-hidden="true"></div>
+                <div class="shadow-lg md:rounded-3xl">
+                <div class="bg-indigo-500 [clip-path:inset(0)] md:[clip-path:inset(0_round_theme(borderRadius.3xl))]">
+                    <div class="absolute -inset-y-px left-1/2 -z-10 ml-10 w-[200%] skew-x-[-30deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white md:ml-20 lg:ml-36" aria-hidden="true"></div>
+                    <div class="relative px-6 pt-8 sm:pt-16 md:pl-16 md:pr-0">
+                    <div class="mx-auto max-w-2xl md:mx-0 md:max-w-none">
+                        <div class="w-screen overflow-hidden rounded-tl-xl bg-gray-900">
+                        <div class="flex bg-gray-800/40 ring-1 ring-white/5">
+                            <div class="-mb-px flex text-sm font-medium leading-6 text-gray-400">
+                            <div class="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">Welcome.jsx</div>
+                            </div>
+                        </div>
+                        <div class="px-6 py-6">
+                        <pre class="text-[0.8125rem] leading-6 text-gray-300" style="tab-size: 2">
+<code>
+<span class="text-indigo-400">function greet</span>() {
+    return (
+        &lt;<span class="text-indigo-400">h1</span>&gt;Hello World&lt;/<span class="text-indigo-400">h1</span>&gt;
+    )
+}
+
+<span>// don't bother to read it</span>
+<span>// it's just another hello world</span>
+</code>
+                        </pre>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="pointer-events-none absolute inset-0 ring-1 ring-inset ring-black/10 md:rounded-3xl" aria-hidden="true"></div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div class="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32"></div>
+        </div>
+  </div>
+</template>
