164fab797c53caabcebf6abc1c195a6db9d7af7a
Emil <kadilli.emil@gmail.com>
2024-03-02 00:58:56 +0100

    eager load post category

--- server/controllers/Post.js
+++ server/controllers/Post.js
@@ -4,7 +4,7 @@ const Post = require('../models/Post');
 // @route   GET /api/posts
 const getPosts = async (req, res) => {
     try {
-        const posts = await Post.find();
+        const posts = await Post.find().populate('category');
         res.status(200).json(posts);
     } catch (err) {
         res.status(500).json(err);
@@ -15,7 +15,7 @@ const getPosts = async (req, res) => {
 // @route GET /api/posts/:id
 const getPost = async (req, res) => {
     try {
-        const post = await Post.findById(req.params.id);
+        const post = await Post.findById(req.params.id).populate('category');
         res.status(200).json(post);
     } catch (err) {
         res.status(500).json(err);
@@ -51,7 +51,7 @@ const updatePost = async (req, res) => {
 // @route DELETE /api/posts/:id
 const deletePost = async (req, res) => {
     try {
-        const post = await Post.findByIdAndDelete(req.params.id);
+        await Post.findByIdAndDelete(req.params.id);
         res.status(200).json('Post has been deleted...');
     } catch (err) {
         res.status(500).json(err);
